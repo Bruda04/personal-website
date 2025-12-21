59268c217df87fdbc470e13637d409e3935dc85a
Luka Bradic <bradicluka04@gmail.com>
2025-12-21 18:34:02 +0100

    [ADD] - base structure

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-zinc-900 text-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold">Tailwind v4 + Vite 🚀</h1>
+    </div>
+  )
+}
